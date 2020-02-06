b615dff16614414ceb8d66fd8e019c0b402efeb6
Hubert Balcerzak <hubertus248@gmail.com>
2020-02-06 11:45:34 +0100

    init

---
+++ src/main/kotlin/pl/starchasers/mdpages/MdpagesApplication.kt
@@ -0,0 +1,11 @@
+package pl.starchasers.mdpages
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MdpagesApplication
+
+fun main(args: Array<String>) {
+    runApplication<MdpagesApplication>(*args)
+}
